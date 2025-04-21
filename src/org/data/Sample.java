cbe6704adfac3cf7a8eeb54c830ee78c20dcc42d
shakthishakthi0108 <shakthi.shakthi0108@gmail.com>
2025-04-21 06:13:54 +0530

    New lines get added

--- src/org/data/Sample.java
+++ src/org/data/Sample.java
@@ -1,6 +1,13 @@
 package org.data;
 
 public class Sample {
+	
+	public void addition(int a, int b) { 
+		
+		int c = a+b;
+		System.out.println("C = "+ c);
+
+	}
 
 	public static void main(String[] args) {
 		
